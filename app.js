0d0d5d432f52e0fad8c533fb6e2605f171b47aa6
A.nassstay <Anastasia.stay@yandex.ru>
2023-06-19 23:05:06 +0300

    errors'

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const cors = require('./middlewares/cors');
 const router = require('./routes/index'); // импорт роутов
 
 const { PORT, DATABASE } = process.env;
-const { DEFAULT_PORT, DEFAULT_DATABASE } = require('./utils/config');
+const { DEFAULT_DATABASE } = require('./utils/config');
 
 const app = express(); // создать приложение методом express
 
@@ -44,4 +44,4 @@ app.use(errorLogger); // логгер ошибок
 app.use(validationErrors()); // обработчик ошибок celebrate
 app.use(errors); // централизованный обработчик ошибок
 
-app.listen(PORT || DEFAULT_PORT); // Слушать порт
+app.listen(PORT); // Слушать порт
