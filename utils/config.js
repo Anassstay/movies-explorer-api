bee6e6dee1f7b50cf14d554075eb9e4ac0257f58
A.nassstay <Anastasia.stay@yandex.ru>
2023-06-22 23:25:46 +0300

    cors

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,7 @@
 const JWT_SECRET = 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b';
-const DEFAULT_PORT = 3000;
 const DEFAULT_DATABASE = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 
 module.exports = {
   JWT_SECRET,
-  DEFAULT_PORT,
   DEFAULT_DATABASE,
 };
